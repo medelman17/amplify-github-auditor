0fdcc89712bb59b85d1d7941a0e7e7810d267ed6
Michael Edelman <metalman@amazon.com>
2021-04-07 18:45:06 -0400

    update webhook

--- pages/api/gh.ts
+++ pages/api/gh.ts
@@ -1,10 +1,11 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 
-import { runMiddleware } from "@/api/utils";
-import { webhookMiddleware } from "@/api/webhooks/github";
+import { runMiddleware } from "../../lib/api/utils";
+import { webhookMiddleware } from "../../lib/api/webhooks/github";
 
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
   await runMiddleware(req, res, webhookMiddleware);
+  res.status(200).json({ name: "John Doe" });
 };
 
 export default handler;

56724cec8bc9f6fa5d59953845befa242b7b84e3
Michael Edelman <metalman@amazon.com>
2021-04-08 17:40:31 -0400

    update pr handler

--- lib/api/webhooks/github/handlers/pullrequests.ts
+++ lib/api/webhooks/github/handlers/pullrequests.ts
@@ -5,4 +5,10 @@ export const prEventHandler: HandlerFunction<"pull_request", unknown> = async ({
   payload,
 }) => {
   const sender = await Models.User.fromEvent(payload.sender);
+  const repository = await Models.Repository.fromEvent(payload.repository);
+  const author = await Models.User.fromEvent(payload.pull_request.user);
+
+  for (const assignee of payload.pull_request.assignees) {
+    const user = await Models.User.fromEvent(assignee);
+  }
 };
